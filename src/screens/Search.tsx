8faf3d41a2461321b7d84ad6d86e1328220df8b1
Alexandar Galabov <alexandar_1997@abv.bg>
2021-05-02 15:01:05 +0300

    feat: hook up UI with search service

--- src/screens/Search.tsx
+++ src/screens/Search.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Paragraph, Button, Portal } from 'react-native-paper';
-// import { EventList } from '../components/Event/List';
+import { EventList } from '../components/Event/List';
 import { SearchPopup } from '../components/SearchPopup';
-import { useCustomNavigation } from '../hooks/use-custom-navigation';
+import { MusicCategory, Event } from '../models';
+import { search } from '../services/search-service';
 import { PageWrapper } from './PageWrapper';
 
 const styles = StyleSheet.create({
@@ -16,44 +17,38 @@ const styles = StyleSheet.create({
 });
 
 export function SearchScreen() {
-  const { navigate, goBack } = useCustomNavigation();
-  // const [events, setEvents] = useState<Event[]>();
+  const [events, setEvents] = useState<Event[]>();
 
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  // useEffect(() => {
-  //   const asyncAction = async () => {
-  //     const fetchedEvents = await search.search({ categories, query, sortBy });
-  //     setEvents(fetchedEvents);
-  //   };
-  //   asyncAction();
-  // }, []);
+  const loadEvents = useCallback(
+    async (categories?: MusicCategory[], sortBy?) => {
+      // TODO: Add query
+      const fetchedEvents = await search.search({ categories, sortBy });
+      setEvents(fetchedEvents);
+    },
+    [],
+  );
+
+  useEffect(() => {
+    loadEvents();
+  }, [loadEvents]);
 
   return (
     <>
       <PageWrapper scrollable>
         <View style={styles.root}>
-          <Paragraph>Search</Paragraph>
-
-          <Button onPress={() => navigate('EventDetails')}>
-            <Paragraph>Go to Event Details</Paragraph>
-          </Button>
-          <Button onPress={() => goBack()}>
-            <Paragraph>Go back</Paragraph>
-          </Button>
-
           <Button onPress={() => setIsModalOpen(true)}>
-            <Paragraph>Open Modal</Paragraph>
+            <Paragraph>Refine</Paragraph>
           </Button>
         </View>
-        {/* {events && <EventList events={events} />} */}
+        {events && <EventList events={events} />}
       </PageWrapper>
       <Portal>
         <SearchPopup
           isOpen={isModalOpen}
           onClose={() => setIsModalOpen(false)}
-          // eslint-disable-next-line no-console
-          onSearchPerform={console.log}
+          onSearchPerform={loadEvents}
         />
       </Portal>
     </>
