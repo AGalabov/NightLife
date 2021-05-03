f118a2f3c19830ea15f3f0189b4a6882bb3ab38f
Alexandar Galabov <alexandar_1997@abv.bg>
2021-05-03 13:19:50 +0300

    refactor: extract colors as constants

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import { RootNavigation } from './src/screens/Navigation';
 import { UserContextProvider } from './src/context/user';
-import { black } from './src/assets/colors';
+import { black, placeholderGray, primary } from './src/assets/colors';
 
 type Theme = typeof DefaultTheme;
 
@@ -15,11 +15,11 @@ const theme: Theme = {
   roundness: 8,
   colors: {
     ...DefaultTheme.colors,
-    primary: '#6979F8',
+    primary,
+    accent: primary,
     text: 'white',
     background: black,
-    accent: '#6979F8',
-    placeholder: '#8C8C8C',
+    placeholder: placeholderGray,
   },
 };
 
